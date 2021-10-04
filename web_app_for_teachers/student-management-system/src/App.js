1c818d0a4bdc5f27f076453b98830c643dcd3ae7
thusharakart <thusharakart@gmail.com>
2021-10-04 09:24:11 +0530

    feat(app): initialize the app structure;

---
+++ web_app_for_teachers/student-management-system/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './components/NavBar.js';
+import TableComponent from './components/Table';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+
+      <TableComponent/>
+
+    </div>
+  );
+}
+
+export default App;
